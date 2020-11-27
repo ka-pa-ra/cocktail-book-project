591a63131da0b00c1fd643f36e17b896af3bb84e
ka-pa-ra <capriolo63@gmail.com>
2020-11-27 00:05:15 +0000

    update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask
 if os.path.exists("env.py"):
     import env
 
-# instance of Flask
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "hello wor"
+    return "Hello World ... again!"
 
 
 if __name__ == "__main__":
